d0916c713c6490989392b5e894953777c71e745b
Tarel-Github <sms_stmax@naver.com>
2023-01-31 14:24:24 +0900

    중간 저장

--- Day02/code06_list.py
+++ Day02/code06_list.py
@@ -1,4 +1,4 @@
-#복합형
+# 복합형
 val1 = 1
 val2 = 2
 val3 = 3
@@ -14,8 +14,8 @@ print(val1 + val2 + val3 + val4 + val5 + val6 + val7 + val8 + val9 + val10)
 
 #위와같은 형태를 방지하기 위해서 아래 자료형을 사용
 
-#리스트 == 배열
-#C언어에서는 배열과 리스트가 따로 있지만 파이썬에서는 둘 다 같은 개념이다.
+# 리스트 == 배열
+# C언어에서는 배열과 리스트가 따로 있지만 파이썬에서는 둘 다 같은 개념이다.
 a= [1,2,3,4,5,6,7,8,9,10]
 sum = 0
 
@@ -46,9 +46,29 @@ print(arr6)
 arr4.append(4) #arr4에 4를 넣는다.
 print(arr4)
 
-#튜플
+# 튜플
 #튜플은 소괄호를 사용
 tuple1 = (1,2,3,4)
 print(tuple1)
 #튜플은 리스트와 다르게 값을 넣고 뺄 수 없다.
+
+
+# 딕셔너리
+spiderman = {'name' : 'Peter Parker',
+              'age' : 18,
+           'weapon' : 'Web Shooter',
+          'deserter': '탈영병'}
+
+print(spiderman)
+print(spiderman['weapon'])
+print(spiderman['age'])
+print(type(spiderman))
+
+#집합
+set1 = set([1,2,3,4])
+print(set1)
+
+set2 = set("Hello World")
+print(set2)
+
 
