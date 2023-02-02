c12b08802069ea25194a63bb1d4c45df3eeef5eb
Tarel-Github <sms_stmax@naver.com>
2023-02-02 17:04:49 +0900

    Update code23_car.py

--- Day04/code23_car.py
+++ Day04/code23_car.py
@@ -1,16 +1,35 @@
 # 자동차 클래스
 class Car:
-    number = '15나 1234'
+    number = '12나 1234'
 
     def get_number(self) -> str:
-        return self.number
+        return self.__number
 
-    def __init__(self) -> None:
-        return ('__init__')
-    def __new__(cls):
-        print('__new__')
-        return super().__new__(cls)
-    def __str__(self) -> str:
-        return f'차번호는 {self.number()} 입니다.'
+    def __init__(self, number = '12나 1234') -> None:
+        self.__number = number
+        print('__init__')
 
+    # def __new__(cls):
+    #     print('__new__')
+    #     return super().__new__(cls)
+
+    # 클래스 외부에선 변경불가, 멤버함수로는 내부조작 가능
+    def set_number(self, number):
+        self.__number = number
         
+    def __str__(self) -> str:
+        return f'내 차번호는 {self.__number} 입니다.'
+
+yourcar = Car(number = '88호 8888')
+print(yourcar)
+yourcar.__number = '바뀐번호 1111' #외부에서는 값을 바꿀 수 없음
+print(yourcar)
+yourcar.set_number = '셋넘버로 바꿈 2222'
+print(yourcar)
+
+# mycar = Car()
+# print(mycar)
+# print(f'제 차는요 아반떼고 번호가 {mycar.get_number()} 입니다.')
+
+# mycar.number = '34나 3456'
+# print(mycar)
