44ccf724736867867bf722ba0c7d27e0d4d01134
Tarel-Github <sms_stmax@naver.com>
2023-02-03 16:08:02 +0900

    에러처리

---
+++ Day05/code36_exception.py
@@ -0,0 +1,15 @@
+# 예외처리
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+x, y = int(input('두 수를 입력하세요 > ').split())
+print('테스트')
+print(add(x, y))
+print(mul(x, y))
+print(div(x, y))
