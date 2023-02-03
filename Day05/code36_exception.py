db687f56a460f81b646d3ef1f8ae08a3344bc79d
Tarel-Github <sms_stmax@naver.com>
2023-02-03 17:14:54 +0900

    예외처리

--- Day05/code36_exception.py
+++ Day05/code36_exception.py
@@ -1,15 +1,24 @@
 # 예외처리
 def add(a, b):
     return a + b
-
 def mul(a, b):
     return a * b
-
 def div(a, b):
     return a / b
 
-x, y = int(input('두 수를 입력하세요 > ').split())
-print('테스트')
-print(add(x, y))
-print(mul(x, y))
-print(div(x, y))
+x, y = input('두 수를 입력하세요 > ').split()
+x = int(x)
+y = int(y)
+
+try:
+    print(div(x, y))
+# except ZeroDivisionError as e:
+#     print('0으로 나눌 수 없음')
+except Exception as e:
+    print(e)
+    #exit()
+finally:
+    print("계산이 완료되었습니다.")
+
+# print(add(x, y))
+# print(mul(x, y))
