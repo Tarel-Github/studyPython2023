efb79a33099fe06dc883fd1432be3a583f258a1f
Tarel-Github <sms_stmax@naver.com>
2023-02-10 12:51:05 +0900

    수정

--- Day10/code58_pyqt_dialog.py
+++ Day10/code58_pyqt_dialog.py
@@ -10,21 +10,23 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-
         self.btnDlg = QPushButton('Dialog', self)
-        self.btnDlg.move(100, 100)
-        self.btnDlg.resize(100, 100)
+        self.btnDlg.move(20, 20)
+        self.btnDlg.clicked.connect(self.onClicked)
 
-
-        #self.setLayout(vbox)    
-
-        self.textEdit = QTextEdit(self)
-
+        self.txtInput = QLineEdit(self)
+        self.txtInput.move(20, 50)
 
         #필수 설정
         self.setWindowTitle('라인에디트')
         self.setGeometry(300, 300, 300, 300)
         self.show()
+
+    def btnClicked(self):
+        text, ok = QInputDialog.getText(self, '인풋다이얼로그', '이름을 적으세요')
+
+        if ok:
+            self.txtInput.setText(text)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
